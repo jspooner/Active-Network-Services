f9948faa058cefe25c23d9d7df4b0306adb082dd
Jonathan Spooner <jspooner@gmail.com>
2010-09-28 10:00:12 -0700

    Adding memcached to Active

--- spec/Active_spec.rb
+++ spec/Active_spec.rb
@@ -1,6 +1,29 @@
-
+# Require the spec helper relative to this file
 require File.join(File.dirname(__FILE__), %w[spec_helper])
+require File.join(File.dirname(__FILE__), %w[custom_matchers_spec])
+require File.join(File.dirname(__FILE__), %w[ .. lib services search])
+require File.join(File.dirname(__FILE__), %w[ .. lib services activity])
+include Active
+include Active::Services
+
 
 describe Active do
+
+  it "shouls set memcache" do
+    Active.CACHE.should be_nil
+    Active.memcache_host "localhost:11211"
+    Active.CACHE.should_not be_nil    
+  end
+  
+  it "should have a working memcache" do
+    Active.memcache_host "localhost:11211"
+    Active.CACHE.set("active","rocks");
+    Active.CACHE.get("active").should eql("rocks")
+  end
+  
+  it "should cache the data" do
+    Active.memcache_host "localhost:11211"
+  end
+  
 end
 
