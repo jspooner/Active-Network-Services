846b7da27c227b4d30c848a46567838a5c38a9d0
Jonathan Spooner <jspooner@gmail.com>
2010-09-29 11:44:16 -0700

    Moved address and updated the gsa call

---
+++ lib/services/address.rb
@@ -0,0 +1,17 @@
+module Active
+  module Services  
+    class Address
+      attr_accessor  :name, :state, :city, :state, :zip, :lat, :lng, :country
+      def initialize(data={})
+        @name    = data[:name]
+        @state   = data[:state]
+        @city    = data[:city]
+        @state   = data[:state]
+        @zip     = data[:zip]
+        @lat     = data[:lat]
+        @lng     = data[:lng]
+        @country = data[:country]
+      end    
+    end
+  end
+end
