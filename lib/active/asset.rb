07caf092689625c4cd5ea23697dbeff9148351da
Jonathan Spooner <jspooner@gmail.com>
2011-06-03 12:36:32 -0700

    Removing comments

--- lib/active/asset.rb
+++ lib/active/asset.rb
@@ -1,22 +1,12 @@
 module Active
-  
   class Asset
     
-    # include Active::QueryMethods # instance
     extend Active::QueryMethods::ClassMethods
     
     include Active::FinderMethods
     extend Active::FinderMethods::ClassMethods
     
     attr_accessor :data
-    # 
-    # def initialize(data={}, options={})
-    #   @options = {
-    #     :s => "relevance",
-    #     :v => "json"
-    #   }
-    #   @options[:f] = options[:facet] if options[:facet]
-    # end
     
   end
 end
